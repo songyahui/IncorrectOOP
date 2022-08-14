3c86f655f0f698abcc7847d2a1b9e46ce02aa0e9
songyahui <songyahuiapp@hotmail.com>
2022-08-14 22:02:40 +0800

    m

--- ast.mli
+++ ast.mli
@@ -1,64 +1,33 @@
 type var = string  (*name of the signal e.g., A B C*)
-type name = string
+type class_name = string
+type mn = string
 
+type spec = SPEC
+type static = spec 
+type dynamic = spec
 
-type signal = One of var | Zero of var
+type tau = Int | Bool | Void | Class of class_name
 
-(*signal set*)
-type instance = signal list ;;
+type constant = INT of int | BOOL of bool | VOID
 
-type p_instance = signal list * signal list ;;
+type value = Const of constant | Null | Variable of var
 
-type p_es = PBot 
-        | PEmp 
-        | PInstance of p_instance 
-        | PCon of p_es * p_es
-        | PDisj of p_es * p_es
-        | PKleene of p_es
-        | POmega of p_es
-        | PNtimed of p_es * int
+type field = var * (var list)
 
-type es = Bot 
-        | Emp 
-        | Instance of instance 
-        | Con of es * es
-        | Disj of es * es
-        | Kleene of es
-        | Omega of es
-        | Ntimed of es * int
+type expression = 
+        | Value of value
+        | Assign of (var * value)
+        | AssignField of (field * value)
+        | Casting of class_name * value
+        | NewObj of class_name * (var list)
+        | Call of field * (var list)
+        | Declear of tau * var 
+        | InstanceOf of value *  class_name
+        | Sequence of expression * expression
+        | IfElse of value * expression * expression
 
-type history = p_es 
-
-type current = p_instance
-
-type trace = history *  current option * name option (*exiting from a trap*) 
-
-type inclusion = es * es;;
-
-type prog = Nothing 
-          | Pause 
-          | Seq of prog * prog 
-          | Par of prog * prog
-          | Loop of prog
-          | Declear of var * prog
-          | Emit of var
-          | Present of var * prog * prog
-          | Trap of name * prog
-          | Exit of name
-          | Run of name
-          | Suspend of prog * name 
-
-type ltl = Lable of string 
-        | Next of ltl
-        | Until of ltl * ltl
-        | Global of ltl
-        | Future of ltl
-        | NotLTL of ltl
-        | Imply of ltl * ltl
-        | AndLTL of ltl * ltl
-        | OrLTL of ltl * ltl
-
-type prog_states = trace list
-
-type spec_prog = name * var list * var list * es * es * prog
-            (* name , input, output, precon, postcon, body*)
+type mType = Virtual | Overide | Inherit
+type formalArguments = ((tau * var) list) 
+type classMeth = mType * tau * mn * formalArguments * static * dynamic * expression 
+type classDefinition = class_name * (class_name option) * formalArguments * (classMeth list) 
+type program = classDefinition list 
